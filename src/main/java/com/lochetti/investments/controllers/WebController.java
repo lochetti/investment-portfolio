ffa1b337c44a3f67aa87b152ecc145af0c5c330d
Renato Lochetti <renato.lochetti@gmail.com>
2024-01-02 18:19:43 +0000

    Use thymeleaf variable

--- src/main/java/com/lochetti/investments/controllers/WebController.java
+++ src/main/java/com/lochetti/investments/controllers/WebController.java
@@ -1,12 +1,17 @@
 package com.lochetti.investments.controllers;
 
+import com.lochetti.investments.models.Asset;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class WebController {
-    @RequestMapping(value = "/index")
-    public String index() {
+    @RequestMapping(value = "/")
+    public String index(Model model) {
+        var asset = new Asset("IWDA");
+
+        model.addAttribute("asset", asset);
         return "index";
     }
 }
---
+++ src/main/java/com/lochetti/investments/models/Asset.java
@@ -0,0 +1,4 @@
+package com.lochetti.investments.models;
+
+public record Asset(String ticker) {
+}
